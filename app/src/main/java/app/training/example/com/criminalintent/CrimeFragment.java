8489afaa26376f94143f7784bfd56548a5e8c771
salmatador <salmatador@moonstub.com>
2015-10-14 08:25:06 -0700

    Wired EditText Widget to Fragment - Wired Fragment to Activity

--- app/src/main/java/app/training/example/com/criminalintent/CrimeFragment.java
+++ app/src/main/java/app/training/example/com/criminalintent/CrimeFragment.java
@@ -3,15 +3,19 @@ package app.training.example.com.criminalintent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * Created by Micah on 10/14/2015.
  */
 public class CrimeFragment extends Fragment {
     private Crime mCrime;
+    private EditText mTitleField;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -23,6 +27,25 @@ public class CrimeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        mTitleField = (EditText) view.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                //Left Blank
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //Left Blank
+            }
+        });
+
         return view;
     }
 }
