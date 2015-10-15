d03552c5ec27b69a42f06bfa3778e0d09b13f8f1
salmatador <salmatador@moonstub.com>
2015-10-14 20:52:16 -0700

    Create The Date Dialog

--- app/src/main/java/app/training/example/com/criminalintent/CrimeFragment.java
+++ app/src/main/java/app/training/example/com/criminalintent/CrimeFragment.java
@@ -2,6 +2,7 @@ package app.training.example.com.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -17,6 +18,7 @@ import java.util.UUID;
 public class CrimeFragment extends Fragment {
 
     private static final String ARG_CRIME_ID = "crime_id";
+    private static final String DIALOG_DATE = "DialogDate";
 
     private Crime mCrime;
     private EditText mTitleField;
@@ -65,7 +67,14 @@ public class CrimeFragment extends Fragment {
 
         mDateButton = (Button) view.findViewById(R.id.crime_date);
         mDateButton.setText(mCrime.getDate().toString());
-        mDateButton.setEnabled(false);
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getFragmentManager();
+                DatePickerFragment dialog = new DatePickerFragment();
+                dialog.show(fragmentManager, DIALOG_DATE);
+            }
+        });
 
         mSolvedCheckBox = (CheckBox) view.findViewById(R.id.crime_solved);
         mSolvedCheckBox.setChecked(mCrime.isSolved());
---
+++ app/src/main/java/app/training/example/com/criminalintent/DatePickerFragment.java
@@ -0,0 +1,24 @@
+package app.training.example.com.criminalintent;
+
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+
+public class DatePickerFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        View view = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_date, null);
+        return new AlertDialog.Builder(getActivity())
+                .setView(view)
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
+    }
+
+}
